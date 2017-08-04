ec2047b5e2a502812e7486fdd092ec167b3c0b8f
Rahul Bhalla Rb <rbpcadmin@test.local>
2017-08-04 16:25:00 +0530

    v1.0t
    v1.0t

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing2
+//
+//  Created by rbpcadmin on 8/4/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
